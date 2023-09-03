a50822549f65a440b7d2575cb532286fb001755d
Luis Gustavo Sebastian Sam <gustavosebastiansam@outlook.com>
2023-09-03 14:33:05 -0500

    Modificación para cumplir reglas checkstyle

--- src/main/java/com/microservice/customer/CustomerApplication.java
+++ src/main/java/com/microservice/customer/CustomerApplication.java
@@ -3,11 +3,14 @@ package com.microservice.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Esta clase permite levantar el microservice de clients.
+ * */
 @SpringBootApplication
 public class CustomerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CustomerApplication.class, args);
+  }
 
 }
