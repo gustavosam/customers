be220cc12c4594ccea108492f69289cbe6508dda
Luis Gustavo Sebastian Sam <gustavosebastiansam@outlook.com>
2023-09-18 16:14:19 -0500

    El MS Clients se registra en el servidor Eureka

--- src/main/java/com/microservice/customer/CustomerApplication.java
+++ src/main/java/com/microservice/customer/CustomerApplication.java
@@ -2,13 +2,16 @@ package com.microservice.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Esta clase permite levantar el microservice de clients.
  * */
+
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
 public class CustomerApplication {
 
   public static void main(String[] args) {
